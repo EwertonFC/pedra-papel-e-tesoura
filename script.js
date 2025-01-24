3b44719c6fdfb0c628ca00065436c78b1afc28f9
EwertonFC <99198148+EwertonFC@users.noreply.github.com>
2025-01-24 20:19:28 -0300

    Criar logica escolha humano e logica rodada
    O jogo precisa permitir que o usuario escolha 1
    das tres opcoes do jogo.
    Apos a escolha o jogo deve comparar a opcao do
    computador e a do humano e ver quem ganha a rodada
    ou se ha um empate.

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
-function getComputerChoice(){
-    let escolha1 = "Tesoura"
-    let escolha2 = "Papel"
-    let escolha3 = "Pedra"
+let pontuacaoHumano = 0
+let pontuacaoComputador = 0 
+
+function jogarRound(humano, computador){
+
+    if (humano === computador){
+        pontuacaoComputador = ++pontuacaoComputador
+        pontuacaoHumano = ++pontuacaoHumano
+        console.log(`Houve um empate! \n1 ponto para cada.\nComputador: ${pontuacaoComputador}\nHumano: ${pontuacaoHumano}`)
+    } else if (humano === "pedra" && computador === "tesoura") {
+        pontuacaoHumano = ++pontuacaoHumano
+        return console.log(`Humano venceu a rodada. Pontuacao atual: ${pontuacaoHumano}`)
+    } else if (humano === "papel" && computador === "pedra") {
+        pontuacaoHumano = ++pontuacaoHumano
+        return console.log(`Humano venceu a rodada. Pontuacao atual: ${pontuacaoHumano}`)
+    } else if (humano === "tesoura" && computador === "papel") {
+        pontuacaoHumano = ++pontuacaoHumano
+        return console.log(`Humano venceu a rodada. Pontuacao atual: ${pontuacaoHumano}`)
+    } else {
+        pontuacaoComputador = ++pontuacaoComputador
+        return console.log(`Computador venceu a rodada. Pontuacao atual: ${pontuacaoComputador}`)
+    }
+}
+
+function getEscolhaComputador(){
+    let escolha1 = "tesoura"
+    let escolha2 = "papel"
+    let escolha3 = "pedra"
     let escolhaAleatoria = Math.floor(Math.random()*3)
 
-    if (escolhaAleatoria === 1){
+    if (escolhaAleatoria === 0){
+        console.log("Computador escolheu: tesoura")
         return escolha1
-    }else if (escolhaAleatoria === 2) {
+    }else if (escolhaAleatoria === 1) {
+        console.log("Computador escolheu: papel")
         return escolha2
     }else {
-        return escolha3
+        console.log("Computador escolheu: pedra")
+        return escolhaAleatoria = escolha3
     }
-    
 }
+
+
+function getEscolhaHumano(){
+    let humanChoice = prompt("Qual opcao: Pedra, Papel ou Tesoura?")
+    console.log(`Voce escolheu: ${humanChoice}`)
+    return humanChoice.toLowerCase()
+}
+
+jogarRound(getEscolhaHumano(), getEscolhaComputador());
