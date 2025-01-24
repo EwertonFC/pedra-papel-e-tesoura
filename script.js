3535ebe8a0121f368a63696e6176fd3e12a64ec3
EwertonFC <99198148+EwertonFC@users.noreply.github.com>
2025-01-24 20:33:08 -0300

    Criar logica para ter 3 rodadadas de jogo
    Eh bom permitir que o jogo tenha 3 rodadas para
    decidir o campeao. Dessa forma fica mais "justo".

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-let pontuacaoHumano = 0
-let pontuacaoComputador = 0 
-
-function jogarRound(humano, computador){
-
-    if (humano === computador){
-        pontuacaoComputador = ++pontuacaoComputador
-        pontuacaoHumano = ++pontuacaoHumano
-        console.log(`Houve um empate! \n1 ponto para cada.\nComputador: ${pontuacaoComputador}\nHumano: ${pontuacaoHumano}`)
-    } else if (humano === "pedra" && computador === "tesoura") {
-        pontuacaoHumano = ++pontuacaoHumano
-        return console.log(`Humano venceu a rodada. Pontuacao atual: ${pontuacaoHumano}`)
-    } else if (humano === "papel" && computador === "pedra") {
-        pontuacaoHumano = ++pontuacaoHumano
-        return console.log(`Humano venceu a rodada. Pontuacao atual: ${pontuacaoHumano}`)
-    } else if (humano === "tesoura" && computador === "papel") {
-        pontuacaoHumano = ++pontuacaoHumano
-        return console.log(`Humano venceu a rodada. Pontuacao atual: ${pontuacaoHumano}`)
-    } else {
-        pontuacaoComputador = ++pontuacaoComputador
-        return console.log(`Computador venceu a rodada. Pontuacao atual: ${pontuacaoComputador}`)
-    }
-}
-
 function getEscolhaComputador(){
     let escolha1 = "tesoura"
     let escolha2 = "papel"
@@ -40,11 +16,51 @@ function getEscolhaComputador(){
     }
 }
 
-
 function getEscolhaHumano(){
     let humanChoice = prompt("Qual opcao: Pedra, Papel ou Tesoura?")
     console.log(`Voce escolheu: ${humanChoice}`)
     return humanChoice.toLowerCase()
 }
 
-jogarRound(getEscolhaHumano(), getEscolhaComputador());
+function jogo(){
+    
+    let pontuacaoHumano = 0
+    let pontuacaoComputador = 0
+    let controlador  = 0 
+    
+    function jogarRound(humano, computador){
+
+        if (humano === computador){
+            pontuacaoComputador = ++pontuacaoComputador
+            pontuacaoHumano = ++pontuacaoHumano
+            console.log(`Houve um empate! \n1 ponto para cada.\nComputador: ${pontuacaoComputador}\nHumano: ${pontuacaoHumano}`)
+        } else if (humano === "pedra" && computador === "tesoura") {
+            pontuacaoHumano = ++pontuacaoHumano
+            return console.log(`Humano venceu a rodada. Pontuacao atual: ${pontuacaoHumano}`)
+        } else if (humano === "papel" && computador === "pedra") {
+            pontuacaoHumano = ++pontuacaoHumano
+            return console.log(`Humano venceu a rodada. Pontuacao atual: ${pontuacaoHumano}`)
+        } else if (humano === "tesoura" && computador === "papel") {
+            pontuacaoHumano = ++pontuacaoHumano
+            return console.log(`Humano venceu a rodada. Pontuacao atual: ${pontuacaoHumano}`)
+        } else {
+            pontuacaoComputador = ++pontuacaoComputador
+            return console.log(`Computador venceu a rodada. Pontuacao atual: ${pontuacaoComputador}`)
+        }
+    }
+
+    while (controlador < 3){
+        controlador = ++controlador
+        jogarRound(getEscolhaHumano(), getEscolhaComputador())
+    }
+    
+    if (pontuacaoHumano > pontuacaoComputador){
+        console.log(`Voce ganhou com uma pontuacao de ${pontuacaoHumano}`)
+    } else if (pontuacaoComputador > pontuacaoHumano) {
+        console.log(`Computador ganhou com uma pontuacao de ${pontuacaoComputador}`)
+    } else {
+        console.log(`Houve um empate onde voce ficou com: ${pontuacaoHumano} e o computador ${pontuacaoComputador}`)
+    }
+}
+
+jogo();
