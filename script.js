55d7c91ac195547359874e2d5a4423bb14fe77b4
EwertonFC <99198148+EwertonFC@users.noreply.github.com>
2025-02-10 21:20:24 -0300

    Ajustar logica do jogo
    eh necessario que quando o jogador clique no botao
    de escolha imediatamente haja uma comparacao com
    as opcoes do jogo para ver quem ganha a rodada.

--- script.js
+++ script.js
@@ -2,13 +2,44 @@ let botao1 = document.querySelector("#btn-1")
 let botao2 = document.querySelector("#btn-2")
 let botao3 = document.querySelector("#btn-3")
 
-botao1.addEventListener("click", function getPedra(){
-    return console.log(botao1.textContent.toLocaleLowerCase())
-})
-botao2.addEventListener("click", function getPapel(){
-    return console.log(botao2.textContent.toLocaleLowerCase())
-})
-botao3.addEventListener("click", function getTesoura(){
-    return console.log(botao3.textContent.toLocaleLowerCase())
-})
+function playRound(){
+    let escolhaHumano = document.activeElement.textContent.toLocaleLowerCase()
+    let escolhaComputador = getComputador()
 
+    if (escolhaHumano === escolhaComputador){
+        return console.log(`empate. Vc escolheu ${escolhaHumano} e o computador tambem escolher ${escolhaComputador}`)
+    } else if (escolhaHumano === "pedra" && escolhaComputador === "papel"){
+        return console.log(`Vc escolheu ${escolhaHumano} e o computador ${escolhaComputador}. Vc perdeu.`)
+    } else if (escolhaHumano === "pedra" && escolhaComputador === "tesoura"){
+        return console.log(`Vc escolheu ${escolhaHumano} e o computador ${escolhaComputador}. Vc ganhou.`)
+    } else if (escolhaHumano === "tesoura" && escolhaComputador === "pedra"){
+        return console.log(`Vc escolheu ${escolhaHumano} e o computador ${escolhaComputador}. Vc perdeu.`)
+    } else if (escolhaHumano === "tesoura" && escolhaComputador === "papel"){
+        return console.log(`Vc escolheu ${escolhaHumano} e o computador ${escolhaComputador}. Vc ganhou.`)
+    } else if (escolhaHumano === "papel" && escolhaComputador === "pedra"){
+        return console.log(`Vc escolheu ${escolhaHumano} e o computador ${escolhaComputador}. Vc ganhou.`)
+    } else if (escolhaHumano === "papel" && escolhaComputador === "tesoura"){
+        return console.log(`Vc escolheu ${escolhaHumano} e o computador ${escolhaComputador}. Vc perdeu.`)
+    } else {
+        console.log("algo de errado nao esta certo")
+        console.log(escolhaComputador)
+        console.log(escolhaHumano)
+    }
+}
+
+function getComputador(){
+    let opcoes = ["pedra", "papel", "tesoura"]
+    let escolhaAleatoria = Math.floor(Math.random()*3)
+
+    if (escolhaAleatoria === 0){
+        return opcoes[0]
+    }else if (escolhaAleatoria === 1) {
+        return opcoes[1]
+    }else {
+        return opcoes [2]
+    }
+}
+
+botao1.addEventListener("click", playRound)
+botao2.addEventListener("click", playRound)
+botao3.addEventListener("click", playRound)
